d4889af304052433bce3b4de0ef41ac617ec4a5c
Frederik Nies <frederik.nies@btc-ag.com>
2025-09-19 08:38:05 +0200

    Add basic calculator functionality with styled inputs and change event handling

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,29 @@
 const nameInputElement = document.getElementById('name');
+
 if (nameInputElement) {
     nameInputElement.addEventListener("change", handleChange)
 }
 
-export function handleChange() {
+function handleChange() {
     const name = document.getElementById('name') as HTMLInputElement;
     console.log(`Change Event ${name.value}`)
+}
+
+const plusButton = document.getElementById('plus');
+
+if (plusButton) {
+    plusButton.addEventListener("click", handleClick)
+}
+
+function handleClick() {
+    const numberOneInputElement = document.getElementById('numberOne') as HTMLInputElement;
+    const numberTwoInputElement = document.getElementById('numberTwo') as HTMLInputElement;
+
+    const a : number = numberOneInputElement.valueAsNumber;
+    const b : number = numberTwoInputElement.valueAsNumber;
+
+    const result: number = a + b;
+
+    const resultElement = document.getElementById('numberThree') as HTMLInputElement;
+    resultElement.setAttribute('value', String(result));
 }
